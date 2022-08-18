9f8cc57640bf5f0f512db06eb50792c876d6b5c4
Sachin <sachinsen249@gmail.com>
2022-08-18 14:12:47 +0530

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,6 @@ function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">{props.title}</Link>
-    {/* <a className="navbar-brand" to="/">{props.title}</a> */}
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -15,11 +14,9 @@ function Navbar(props) {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <Link className="nav-link" aria-current="page" to="/">Home</Link>
-          {/* <a className="nav-link" aria-current="page" to="/">Home</a> */}
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/about">{props.about}</Link>
-          {/* <a className="nav-link" to="/about">{props.about}</a> */}
         </li>
       </ul>
       <div className={`form-check form-switch text-${props.mode==='light'?'#2C3E50':'light'}`}>
